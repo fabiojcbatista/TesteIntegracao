1fdc6bf9f3c7d71106dd98544b01c162d30cc3fd
Fabio Jr <fabiojcbweb@gmail.com>
2022-07-26 18:43:33 -0300

    Test: aula 03

--- src/test/java/br/com/alura/leilao/util/JPAUtil.java
+++ src/test/java/br/com/alura/leilao/util/JPAUtil.java
@@ -5,11 +5,11 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtil {
-	
-	private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("tests"); 
+
+	private static EntityManagerFactory factory = Persistence
+			.createEntityManagerFactory("tests");
 
 	public static EntityManager getEntityManager() {
 		return factory.createEntityManager();
 	}
-
 }
